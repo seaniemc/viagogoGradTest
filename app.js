7dca0738e5fd42e8a979339108c3b869923c826e
Sean McGrath <Sean McGrath>
2017-04-15 12:01:57 +0100

    added drawDot and drawLine methods

--- app.js
+++ app.js
@@ -35,4 +35,39 @@ myMap.controller('CanvasCtrl', function($scope){
         draw($scope.data);
         console.log($scope.data);
     }
+
+     function draw(data) {
+        for(var i=0; i<data.length; i++) {
+            drawDot(data[i]);
+            if(i > 0) {
+                drawLine(data[i], data[i-1]);
+            }
+        }
+    }
+    
+    function drawDot(data) {
+        context.beginPath();
+        context.arc(data.x, data.y, data.amount, 0, 2*Math.PI, false);
+        context.fillStyle = "#ccddff";
+        context.fill();
+        context.lineWidth = 1;
+        context.strokeStyle = "#666666";
+        context.stroke();  
+    }
+    
+    function drawLine(data1, data2) {
+        context.beginPath();
+        context.moveTo(data1.x, data1.y);
+        context.lineTo(data2.x, data2.y);
+        context.strokeStyle = "black";
+        context.stroke();
+    }
+    
+    // setup
+    canvas.width = 600;
+    canvas.height = 400;
+    context.globalAlpha = 1.0;
+    context.beginPath();
+    draw($scope.data);    
+
 });
