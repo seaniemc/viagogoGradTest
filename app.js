baa562284ff1e431823a07141fcdc62f5ed25b3a
Sean McGrath <Sean McGrath>
2017-04-19 00:20:00 +0100

    added comments

--- app.js
+++ app.js
@@ -37,9 +37,8 @@ myMap.controller('CanvasCtrl', ['$scope','$state','$stateParams',function Canvas
     var maxTickets = 10;
     $scope.answerArr = [];
     
-
     var userData = $state.params.coor;
-   var obj = JSON.parse(userData);
+    var obj = JSON.parse(userData);
    
    console.log(obj);
    console.log("canvas");
@@ -52,15 +51,17 @@ myMap.controller('CanvasCtrl', ['$scope','$state','$stateParams',function Canvas
             _id: "00"+ i,
 			x: Math.floor(Math.random()*W), //x-coordinate
 			y: Math.floor(Math.random()*H), //y-coordinate
-            //creates a random array of between zero and 30 with random values between 
+            //creates a random array of between 1 and 30 with random values between 
+            //0 and 150
 			ticksPrice: randomArray(Math.floor(Math.random()*30 + 1), 150), 
             dist: 0
 		    });
 	    }
         
     } // End func
-    //userData
+
     //http://stackoverflow.com/questions/5836833/create-a-array-with-random-values-in-javascript
+    //Creates an array of random length with random values
     function randomArray(length, max) {
         return Array.apply(null, Array(length)).map(function() {
             return Math.round(Math.random() * max);
@@ -68,6 +69,7 @@ myMap.controller('CanvasCtrl', ['$scope','$state','$stateParams',function Canvas
     }
 
     //https://xlinux.nist.gov/dads/HTML/manhattanDistance.html
+    //This function calculates the Manhattan distance for each ticket event
     $scope.calculateManhattonDist = function(userData){
         console.log(userData);
         for(var i = 0; i < tickets.length; i++){
@@ -93,15 +95,14 @@ myMap.controller('CanvasCtrl', ['$scope','$state','$stateParams',function Canvas
         var asnwerArr = []
         for (var i = 0; i <= 4; i++){
             var ticket = tickets[i];
+            //sorts the prices array from low to high
             ticket.ticksPrice.sort(function(a, b){return a-b});
             asnwerArr.push(ticket);
         }
         $scope.answerArr = asnwerArr;
         console.log($scope.answerArr);
-        //return asnwerArr;
-    }
-    
 
+    }
     
     $scope.fillTicketsArray(tickets);
     $scope.calculateManhattonDist(obj);
@@ -111,11 +112,12 @@ myMap.controller('CanvasCtrl', ['$scope','$state','$stateParams',function Canvas
 
 myMap.controller('EnterCtrl',['$scope', '$state', function EnterCtrl($scope,$state){
     $scope.formData = {
-        x: 0,
-        y: 0
+        x: '',
+        y: ''
     };
     var userData = $scope.formData;
    
+   //passes the x and y coordinates to the canvas ctrl
     $scope.goToAnswer = function(){
         $state.go('answer', {'coor': JSON.stringify(userData)});
     };
